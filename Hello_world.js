8879d1cf3f9ce442dbc8cf699cc2663a1b20ccac
MarchVlad <ktotogdeto849@gmail.com>
2021-12-13 14:35:56 +0200

    Add /hello-world page

--- Hello_world.js
+++ Hello_world.js
@@ -1,6 +1,16 @@
-console.log('First way:');
-console.log('Hello, world!');
+const http = require("http");
+ 
+http.createServer(function(request, response){
+    response.setHeader("Content-Type", "text/html; charset=utf-8;");
 
-var text = 'Hello, world!';
-console.log('Second way:');
-console.log(text);
+    if(request.url === "/hello-world")
+    {
+        response.write("<h2>Hello world!</h2>");
+    } 
+    else
+    {
+        response.write("<h1>Not found</h1>");
+    }
+
+    response.end();
+}).listen(3000);

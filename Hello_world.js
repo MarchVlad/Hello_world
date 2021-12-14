94b6c9673a43dbcd4486eee251a1c0bc80ce0f70
MarchVlad <ktotogdeto849@gmail.com>
2021-12-14 20:43:53 +0200

    Add count of visits

--- Hello_world.js
+++ Hello_world.js
@@ -1,16 +1,17 @@
-const http = require("http");
- 
-http.createServer(function(request, response){
-    response.setHeader("Content-Type", "text/html; charset=utf-8;");
+const express = require('express');
+const app = express();
 
-    if(request.url === "/hello-world")
-    {
-        response.write("<h2>Hello world!</h2>");
-    } 
-    else
-    {
-        response.write("<h1>Not found</h1>");
-    }
+let data = 0;
+data = Number(data);
 
-    response.end();
-}).listen(3000);
+app.get('/hello-world', function(request, response, next){
+    data = data + 1;
+    console.log(data);
+    response.send(`Amount of your visits: ${String(data)}`);
+})
+
+app.get('/*', function (request, response) {
+    response.sendStatus(404); 
+});
+
+app.listen(3000, () => console.log('The server is running port 3000...'));

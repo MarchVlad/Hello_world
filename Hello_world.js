c478c2a1e431dbf73a02c28af02be39996ea99f7
MarchVlad <ktotogdeto849@gmail.com>
2021-12-17 01:16:50 +0200

    Repair mistakes

--- Hello_world.js
+++ Hello_world.js
@@ -8,22 +8,41 @@ app.use(cookieParser());
 let data = 1;
 
 app.get('/hello-world', (request, response) => {
+    // First method
+    //------------
+
+    // if(request.cookies['count'])
+    // {
+    //     data = request.cookies['count'];
+    // }
+    // if(data === 1)
+    // {
+    //     response.cookie('count', data).send();
+    //     data++;
+    // }
+    // else
+    // {
+    // data = request.cookies['count'];
+    // data++;
+    // response.cookie('count', data);
+    // response.send(`Count:${request.cookies['count']}`);
+    // console.log('Cookies:', request.cookies);
+    // }
+
+    // Second method
+    //------------
+
     if(request.cookies['count'])
     {
         data = request.cookies['count'];
-    }
-    if(data === 1)
-    {
-        response.cookie('count', data).send();
         data++;
+        response.cookie('count', data);
+        response.send(`Count:${request.cookies['count']}`);
+        console.log('Cookies:', request.cookies);
     }
     else
     {
-    data = request.cookies['count'];
-    data++;
-    response.cookie('count', data);
-    response.send(`Count:${request.cookies['count']}`);
-    console.log('Cookies:', request.cookies);
+        response.cookie('count', data).send();
     }
 })
 
